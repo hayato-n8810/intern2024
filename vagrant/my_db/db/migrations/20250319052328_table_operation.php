6cae0415bcb3f5637416b77851c2ed6e9e2b0d6e
hayato-n8810 <s276185@wakayama-u.ac.jp>
2025-03-19 14:58:31 +0900

    マイグレーションの設定を追加

---
+++ vagrant/my_db/db/migrations/20250319052328_table_operation.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class TableOperation extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        // trx_usesテーブルを作成 
+        $userTable = $this->table('trx_users');
+        $userTable->addColumn('user_name', 'string')
+            ->addColumn('password', 'string')
+            ->create();
+
+        // trx_commentsテーブルを作成
+        $commentTable = $this->table('trx_comments');
+        $commentTable->addColumn('user_id', 'integer')
+            ->addColumn('text', 'string')
+            ->create();
+    }
+}
